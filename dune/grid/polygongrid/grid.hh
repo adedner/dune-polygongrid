5d3b110e052c1d46b484937015e7afd54c261488
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2015-03-27 16:29:45 +0100

    migrate grid family into separate file

--- dune/grid/polygongrid/grid.hh
+++ dune/grid/polygongrid/grid.hh
@@ -5,7 +5,7 @@
 
 #include <dune/grid/common/grid.hh>
 
-#include <dune/grid/polygongrid/declaration.hh>
+#include <dune/grid/polygongrid/gridfamily.hh>
 
 namespace Dune
 {
@@ -23,35 +23,18 @@ namespace Dune
 
 
 
-  // PolygonGridFamily
-  // -----------------
-
-  template< class ct, class idx, class Comm >
-  struct PolygonGridFamily
-  {
-    struct Traits
-    {
-      typedef ct ctype;
-      typedef idx Index;
-
-      typedef PolygonGrid< ct, Comm > Grid;
-    };
-  };
-
-
-
   // PolygonGrid
   // -----------
 
   template< class ct, class idx, class Comm >
   class PolygonGrid
-  : public GridDefaultImplementation< 2, 2, ct, PolygonGridFamily< ct, idx, Comm > >
+    : public GridDefaultImplementation< 2, 2, ct, __PolygonGrid::GridFamily< ct, idx, Comm > >
   {
     typedef PolygonGrid< ct, idx, Comm > This;
-    typedef GridDefaultImplementation< 2, 2, ct, PolygonGridFamily< ct, idx, Comm > > Base;
+    typedef GridDefaultImplementation< 2, 2, ct, __PolygonGrid::GridFamily< ct, idx, Comm > > Base;
 
   public:
-    typedef PolygonGridFamily< ct, idx, Comm > GridFamily;
+    typedef __PolygonGrid::GridFamily< ct, idx, Comm > GridFamily;
 
     typedef typename GridFamily::Traits Traits;
 
---
+++ dune/grid/polygongrid/gridfamily.hh
@@ -0,0 +1,35 @@
+#ifndef DUNE_POLYGONGRID_GRIDFAMILY_HH
+#define DUNE_POLYGONGRID_GRIDFAMILY_HH
+
+#include <dune/common/parallel/mpicollectivecommunication.hh>
+
+#include <dune/grid/common/grid.hh>
+
+#include <dune/grid/polygongrid/declaration.hh>
+
+namespace Dune
+{
+
+  namespace __PolygonGrid
+  {
+
+    // GridFamily
+    // ----------
+
+    template< class ct, class idx, class Comm >
+    struct GridFamily
+    {
+      struct Traits
+      {
+        typedef ct ctype;
+        typedef idx Index;
+
+        typedef Dune::PolygonGrid< ct, idx, Comm > Grid;
+      };
+    };
+
+  } // namespace __PolygonGrid
+
+} // namespace Dune
+
+#endif // #ifndef DUNE_POLYGONGRID_GRIDFAMILY_HH
