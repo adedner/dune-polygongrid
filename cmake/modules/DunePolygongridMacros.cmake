cd5f60d7d2498ab6055a65852081471221a3259f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-03-03 15:06:02 +0100

    also include grid factory automatically

--- cmake/modules/DunePolygongridMacros.cmake
+++ cmake/modules/DunePolygongridMacros.cmake
@@ -2,4 +2,4 @@ dune_define_gridtype( GRID_CONFIG_H_BOTTOM
                       GRIDTYPE POLYGONGRID
                       ASSERTION "(GRIDDIM == 2) && (WORLDDIM == 2)"
                       DUNETYPE "Dune::PolygonGrid< double >"
-                      HEADERS "dune/polygongrid/grid.hh" "dune/polygongrid/dgf.hh" )
+                      HEADERS "dune/polygongrid/grid.hh" "dune/polygongrid/gridfactory.hh" "dune/polygongrid/dgf.hh" )
